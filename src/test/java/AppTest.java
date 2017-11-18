4520598a14f609091cbb3ea8a456f07e088a48ae
Gurwinder Pal Singh <gurwinderpalsingh91@gmail.com>
2017-11-18 13:41:45 +0000

    made changes in test file and add gitignore file

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,13 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+import static java.lang.System.out;
 public class AppTest 
 {
 	@Test
 	public void testApp()
     {
         assertEquals(0,new App().calculateSomething());
+        out.println("First maven project is here :-)");
     }
 }
